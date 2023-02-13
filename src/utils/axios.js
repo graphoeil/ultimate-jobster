ad6967b09e4c33353eb728e16484dd28b013d7a9
graphoeil <graphoeil@gmail.com>
2023-02-13 15:27:49 +0100

    Add local url for back-end course

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -4,7 +4,9 @@ import axios from "axios";
 // Custom instance of axios
 // To avoid having to enter the base url for each request ,-)
 const customAxiosFetch = axios.create({
-	baseURL:'https://jobify-prod.herokuapp.com/api/v1/toolkit'
+	baseURL:'https://jobify-prod.herokuapp.com/api/v1/toolkit',
+	// For local dev with back-end (node)
+	// baseURL:'/api/v1'
 });
 
 // Export
